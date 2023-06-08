893566cc100196ddcfa4c9a5df3e8bd0053c14ef
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2023-06-08 11:12:50 +0300

    removed unused test files

--- src/app/modules/budget-tracker/budget-tracker.component.ts
+++ src/app/modules/budget-tracker/budget-tracker.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { BudgetTrackerFacadeService } from './services';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-budget-tracker',
   templateUrl: './budget-tracker.component.html',
-  styleUrls: ['./budget-tracker.component.scss'],
 })
 export class BudgetTrackerComponent implements OnInit {
+  @HostBinding('class')
+  private readonly classes = 'flex h-full';
+
   isLoading$: Observable<boolean>;
 
   constructor(private btFacade: BudgetTrackerFacadeService) {}
