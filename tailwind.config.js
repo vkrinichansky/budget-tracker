fd0daea564c034c3f0ce04f8d431747fc3cef1a8
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2023-06-08 10:31:25 +0300

    fixed navbar item colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,22 @@ const colors = {
   transparent: 'rgb(0, 0, 0, 0)',
 
   white: '#ffffff',
-
   grey: '#636766',
+  'dirty-white': '#F0F2F2',
 
   charcoal: '#2C4251',
   blue: '#395B72',
   'light-blue': '#E2F5FD',
 
-  'dark-green': '#109279',
   'light-green': '#E4FCF7',
+  green: '#24B563',
+  'dark-green': '#109279',
   'hover-green': '#0B7863',
 
-  'red': '#FF6B69',
+  red: '#FF6B69',
   'light-red': '#FFECEC',
 
-  'dirty-white': '#F0F2F2',
-
   'hover-white': 'rgb(255, 255, 255, 0.1)',
-
   'hover-black': 'rgb(0, 0, 0, 0.1)',
 };
 
