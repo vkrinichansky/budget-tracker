92f9d8ce81387ec283e793f004cef5c3297530a4
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2023-07-29 20:08:46 +0300

    markup improve and added vibrate

--- src/app/modules/design-system/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/modules/design-system/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ConfirmationModalData } from '../../models';
 
@@ -7,13 +7,17 @@ import { ConfirmationModalData } from '../../models';
   templateUrl: './confirmation-modal.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfirmationModalComponent {
+export class ConfirmationModalComponent implements OnInit {
   private readonly rootTranslationKey = 'confirmationModal';
 
   constructor(
     private dialogRef: MatDialogRef<ConfirmationModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ConfirmationModalData
   ) {}
+
+  ngOnInit(): void {
+    navigator.vibrate(100);
+  }
 
   buildTranslationKey(key: string): string {
     return `${this.rootTranslationKey}.${key}`;
