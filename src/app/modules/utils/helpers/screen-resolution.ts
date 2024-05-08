26ec661497e45718d8e823121aae2498b6470b92
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2024-05-08 17:27:45 +0300

    improvements

--- src/app/modules/utils/helpers/screen-resolution.ts
+++ src/app/modules/utils/helpers/screen-resolution.ts
@@ -1,7 +1,5 @@
 const mobileResolution = 480;
 
 export function isMobileWidth() {
-  const screenWidth = window.devicePixelRatio * screen.width;
-
-  return screenWidth <= mobileResolution;
+  return screen.width <= mobileResolution;
 }
