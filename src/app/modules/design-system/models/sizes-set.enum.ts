8d1a21bd23315442d6e9d076010704106ffbae32
Andrey Kulyk <>
2024-05-27 19:40:09 +0300

    Finished design

--- src/app/modules/design-system/models/sizes-set.enum.ts
+++ src/app/modules/design-system/models/sizes-set.enum.ts
@@ -1,2 +1,2 @@
-export type IconSize = 'small' | 'medium' | 'big' | 'auto';
+export type IconSize = 'small' | 'medium' | 'big' | 'auto' | 'full-width';
 export type ButtonSize = 'tiny' | 'small' | 'medium' | 'big' | 'large' | 'full' | 'auto';
