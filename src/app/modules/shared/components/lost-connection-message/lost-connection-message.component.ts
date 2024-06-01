d75a5cd775960f6a8868fffdcfe9c3090757a2dd
Andrey Kulyk <>
2024-06-01 13:34:11 +0300

    Button click callback

--- src/app/modules/shared/components/lost-connection-message/lost-connection-message.component.ts
+++ src/app/modules/shared/components/lost-connection-message/lost-connection-message.component.ts
@@ -7,12 +7,15 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LostConnectionMessageComponent {
-  @HostBinding('class') 
-  private readonly classes = 'flex justify-center items-center h-full w-full bg-black-transparent absolute top-0 left-0';
+  @HostBinding('class')
+  private readonly classes =
+    'flex justify-center items-center h-full w-full bg-black-transparent absolute top-0 left-0';
 
   buildTranslationKey(key: string): string {
     return `lostConnectionMessage.${key}`;
   }
+
+  reload(): void {
+    window.location.reload();
+  }
 }
-
-
