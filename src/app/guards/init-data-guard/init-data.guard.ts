80e49e8b26c470c2f2c901de918807acafa661f3
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2024-12-30 12:32:33 +0200

    fix bug with redirect and loader

--- src/app/guards/init-data-guard/init-data.guard.ts
+++ src/app/guards/init-data-guard/init-data.guard.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AuthFacadeService } from '@budget-tracker/auth';
 import { DataInitFacadeService } from '@budget-tracker/data';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitDataGuard {
+  private readonly authFacade = inject(AuthFacadeService);
   constructor(private dataInitFacade: DataInitFacadeService) {}
 
-  async canActivate(): Promise<boolean> {
-    await this.dataInitFacade.initData();
-    return true;
+  canActivate(): Observable<boolean> {
+    return this.authFacade.isLoggedIn().pipe(
+      map((isLoggedIn) => {
+        if (isLoggedIn) {
+          this.dataInitFacade.initData();
+          return true;
+        }
+
+        return false;
+      })
+    );
   }
 }
