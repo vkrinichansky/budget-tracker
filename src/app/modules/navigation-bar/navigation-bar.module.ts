f3850e5cfb468a73dd946844828f717bd5b56076
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2025-01-03 14:46:59 +0200

    UTs preparation

--- src/app/modules/navigation-bar/navigation-bar.module.ts
+++ src/app/modules/navigation-bar/navigation-bar.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { DesignSystemModule } from '@budget-tracker/design-system';
 import { AuthCoreModule } from '@budget-tracker/auth';
-import { NavigationBarItemComponent } from './components/navigation-bar-item/navigation-bar-item.component';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { DataModule } from '@budget-tracker/data';
+import { NavigationBarComponent, NavigationBarItemComponent } from './components';
 
 @NgModule({
   declarations: [NavigationBarComponent, NavigationBarItemComponent],
