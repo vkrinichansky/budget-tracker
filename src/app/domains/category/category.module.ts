4cc0b98fbc9a8957a15f64fbe10e515cd9773b59
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2025-10-24 18:08:38 +0300

    deps clean up

--- src/app/domains/category/category.module.ts
+++ src/app/domains/category/category.module.ts
@@ -18,7 +18,6 @@ import { DesignSystemModule } from '@budget-tracker/design-system';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UtilsModule } from '@budget-tracker/shared-utils';
-import { NgChartsModule } from 'ng2-charts';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MetadataDomainModule } from '@budget-tracker/metadata';
 import { NgxApexchartsModule } from 'ngx-apexcharts';
@@ -34,7 +33,6 @@ import { CategoryEffects, categoryReducer, featureKey } from './store';
     FormsModule,
     ReactiveFormsModule,
     UtilsModule,
-    NgChartsModule,
     ScrollingModule,
     MetadataDomainModule,
     NgxApexchartsModule,
